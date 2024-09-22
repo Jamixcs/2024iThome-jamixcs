43b800d4de1f974f4f5ddc3b8e362203ad8bce90
Jia Guo <jamixcs@gmail.com>
2024-09-22 18:33:25 +0800

    feat: day9

---
+++ src/components/day9/expression.vue
@@ -0,0 +1,11 @@
+<script setup>
+const a = 1;
+let isOK = true;
+</script>
+
+<template>
+  <div>{{ a / 2 }}</div>
+  <div>{{ Math.floor(Math.random() * 10) }}</div>
+  <div>{{ isOK ? "YES" : "NO" }}</div>
+  <div>{{ [0, 1, 2, 3, 4].map((i) => i + 1).join(",") }}</div>
+</template>
---
+++ src/components/day9/method.vue
@@ -0,0 +1,17 @@
+<template>
+  <time :title="toTitleDate(date)" :datetime="date">
+    {{ formatDate(date) }}
+  </time>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const date = ref(new Date());
+const formatDate = (date) => {
+  return date.toLocaleDateString();
+};
+const toTitleDate = (date) => {
+  return date.toLocaleString();
+};
+</script>
