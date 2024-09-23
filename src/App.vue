290855b3a094c2defdf02f4c0f765ebdd06ffec6
Jia Guo <jamixcs@gmail.com>
2024-09-23 23:08:34 +0800

    feat: day10

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,32 @@
 import mustache from "./components/day7/Mustache.vue";
 import vHtml from "./components/day7/vHtml.vue";
 import vBind from "./components/day8/vBind.vue";
+import expression from "./components/day9/expression.vue";
+import method from "./components/day9/method.vue";
+import directives from "./components/day10/directives.vue";
 </script>
 
 <template>
   <main>
+    <h2>Day7</h2>
     <mustache />
     <vHtml rawHtml="<strong>This is where I can put the HTML code</strong>" />
+    <h2>Day8</h2>
     <vBind />
+    <h2>Day9</h2>
+    <expression />
+    <method />
+    <h2>Day10</h2>
+    <directives />
   </main>
 </template>
+
+<style>
+h2 {
+  background-color: black;
+  color: white;
+  text-align: center;
+  padding: 10px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/day10/directives.vue
@@ -0,0 +1,23 @@
+<template>
+  <p v-if="truthyValue">Here is a "truthy value" of v-if example.</p>
+  <p v-if="falsyValue">Here is a "falsy value" of v-if example.</p>
+  <img :src="imagePath" />
+  <br />
+  <button @[eventName]="doSomething">Click Me!!!!</button>
+</template>
+
+<script setup>
+const truthyValue = 123;
+const falsyValue = "";
+const imagePath = "src/components/day10/images/Jerry_watching.jpg";
+const eventName = "click";
+const doSomething = () => {
+  console.log(`You are the BEST`);
+};
+</script>
+
+<style>
+img {
+  width: 300px;
+}
+</style>
